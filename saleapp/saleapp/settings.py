e71143d33f2dff11ec222e6caa3c4497ee39fc46
onggiabayluon <livipst2@gmail.com>
2022-03-20 17:27:54 +0700

    Feat: Login Register - User can now create new account - User can using their account to login

--- saleapp/saleapp/settings.py
+++ saleapp/saleapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopping'
+    'shopping',
+    'accounts'
 ]
 
 MIDDLEWARE = [
