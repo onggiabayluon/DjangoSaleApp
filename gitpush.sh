ad5d19dbd76fee9e8f7ad53890cca104231c0d6c
onggiabayluon <livipst2@gmail.com>
2022-03-25 14:55:19 +0700

    Fix: Fix gitpush.sh file

--- gitpush.sh
+++ gitpush.sh
@@ -6,21 +6,21 @@ reset=`tput sgr0`
 # git checkout -b "$1"
 if [ "$1" != "" ]
 then
-  echo -e "${green}>>> Git checkout -b "$1" \n"
   git checkout -b "$1"
+  echo -e "${green}>>> Git checkout -b "$1" \n"
 # else 
 #   git checkout master
 #   echo -e "${green}>>> Git checkout master"
 fi
 
 # git add .
-echo -e "${green}>>> Git add . \n"
 git add .
+echo -e "${green}>>> Git add . \n"
 
 # git commit -m "$2"
-echo -e "${green}>>> Git commit -m "$2" \n"
 git commit -m "$2"
+echo -e "${green}>>> Git commit -m "$2" \n"
 
 # git push
-echo -e "${green}>>> Git push origin head \n"
 git push origin head
+echo -e "${green}>>> Git push origin head \n"
